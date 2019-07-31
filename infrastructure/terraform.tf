4006904ca7f406a814e7090bc2c1ada63336b054
Theron Luhn <theron@luhn.com>
2019-07-31 14:03:12 -0700

    Get Terraform up and running.

---
+++ infrastructure/terraform.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2.21"
+  profile = "personal"
+}
+
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket     = "luhn-terraform"
+    profile    = "personal"
+    key        = "luhn.com.json"
+    region     = "us-west-2"
+  }
+}
+
+data "aws_caller_identity" "main" {
+}
+
+data "aws_region" "current" {
+}
+
