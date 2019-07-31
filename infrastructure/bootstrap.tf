da64669324063b303a81621c14a2bcd196bd73ce
Theron Luhn <theron@luhn.com>
2019-07-31 14:10:07 -0700

    Bootstrap terraform resources.

---
+++ infrastructure/bootstrap.tf
@@ -0,0 +1,15 @@
+/* Bootstrap the resources that I'm using to run Terraform. */
+
+resource "aws_s3_bucket" "state" {
+	bucket = "luhn-terraform"
+	versioning { enabled = true }
+}
+
+resource "aws_iam_user" "self" {
+  name = "self"
+}
+
+resource "aws_iam_user_policy_attachment" "self_admin" {
+  user       = aws_iam_user.self.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
