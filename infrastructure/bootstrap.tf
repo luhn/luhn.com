59842b742464d1eb2aa6cae6e6626656b4408dd4
Theron Luhn <theron@luhn.com>
2021-04-12 23:27:30 -0700

    Block public access to S3 buckets.

--- infrastructure/bootstrap.tf
+++ infrastructure/bootstrap.tf
@@ -5,6 +5,15 @@ resource "aws_s3_bucket" "state" {
   versioning { enabled = true }
 }
 
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_iam_user" "self" {
   name = "self"
 }
--- infrastructure/site.tf
+++ infrastructure/site.tf
@@ -4,6 +4,15 @@ resource "aws_s3_bucket" "site" {
   bucket = "luhn.com"
 }
 
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_object" "index" {
   bucket       = aws_s3_bucket.site.id
   key          = "index.html"
