68342f4e09ab1120dafeedf5cec3d1337fe8f0d9
Theron Luhn <theron@luhn.com>
2021-04-12 23:21:54 -0700

    Use jsonencode for policy instead of data block.

--- infrastructure/site.tf
+++ infrastructure/site.tf
@@ -30,31 +30,29 @@ resource "aws_s3_bucket_object" "keybase" {
 
 /* Bucket permissions */
 
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.site.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.site.iam_arn]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.site.arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.site.iam_arn]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
-  policy = data.aws_iam_policy_document.s3_policy.json
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.site.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.site.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.site.iam_arn
+        }
+        Action   = "s3:ListBucket"
+        Resource = aws_s3_bucket.site.arn
+      },
+    ]
+  })
 }
 
 /* CloudFront */
